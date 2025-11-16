3d3f77a27df74f7f71126a191240697fbed4e2a9
Anders H <anders@hogqvist.se>
2025-11-16 20:14:33 +0100

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly attributes such as title, company, product, copyright, and version were removed from Properties/AssemblyInfo.cs and added to NRobot.Server.Test.csproj. This centralizes assembly metadata in the project file, streamlining configuration and reducing redundancy.

--- NRobot.Server.Test/Properties/AssemblyInfo.cs
+++ NRobot.Server.Test/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NRobot.Server.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NRobot")]
-[assembly: AssemblyProduct("NRobot.Server.Test")]
-[assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c2be8c37-30e7-4ff6-ba8d-782648275225")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
